5ed6fea66f336fc9b91f40db5ee7facc4459a87b
Stephsas <stephanie0marchand@gmail.com>
2024-04-26 11:20:01 +0200

    etape3

--- js/exo.js
+++ js/exo.js
@@ -1,1 +1,69 @@
-console.log('test 1...2...1...2');
+
+//resoudre le probleme de lien
+console.log('test' + "1...2...1...2");
+
+//on crée l'objet qui va contenir les informations sur Hercule
+
+const hercule = {
+    name: "Hercule",
+    job: "Demi-dieu",
+    age: 35,
+    department: 75,
+    arm: 60.5,
+    inRelationship: true,
+};
+
+function fillProfil(profil) {
+    const listElem = document.getElementById('profil');
+    listElem.classList.remove('hidden');
+    if (!profil || typeof profil !== 'object') {
+        listElem.innerHTML = '<li class="red">Tu as bien appelé <code>fillProfil</code> mais tu ne sembles pas avoir passé d\'objet en argument</li>';
+    }
+    else {
+        if (
+            profil.name === undefined
+            || profil.inRelationship === undefined
+            || profil.job === undefined
+            || profil.department === undefined
+            || profil.arm === undefined
+            || profil.age === undefined
+        ) {
+            listElem.innerHTML = '<li class="yellow">Pour un meilleur affichage essaye de respecter les noms de propriétés donnés dans l\'énoncé et n\'oublie aucune propriété</li>';
+        }
+        else if (
+            typeof profil.name !== 'string'
+            || typeof profil.inRelationship !== 'boolean'
+            || typeof profil.job !== 'string'
+            || typeof profil.age !== 'number'
+            || (typeof profil.department !== 'number' && typeof profil.department !== 'string')
+            || typeof profil.arm !== 'number') {
+            listElem.innerHTML = '<li class="yellow">Ça fonctionne mais certains types de valeurs ne sont pas idéalement choisis</li>';
+        }
+        for (const key in profil) {
+            const newElem = document.createElement('li');
+            newElem.className = 'tags__item';
+            let text;
+            switch (key) {
+                case 'inRelationship':
+                    text = profil[key] ? 'En couple' : 'Célibataire';
+                    break;
+                case 'age':
+                    text = profil[key] + ' ans';
+                    break;
+                case 'department':
+                    text = 'Vit actuellement dans le ' + profil[key];
+                    break;
+                case 'arm':
+                    text = profil[key] + 'cm de tour de bras';
+                    break;
+                default:
+                    text = profil[key];
+            }
+            newElem.innerHTML = text;
+            listElem.appendChild(newElem);
+        }
+    }
+};
+
+fillProfil(hercule);
+
